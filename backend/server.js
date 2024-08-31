01e5acfb07bf42628a125cc641201e324b31b2e6
daveethe <111148401+daveethe@users.noreply.github.com>
2024-08-31 11:50:53 +0200

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Importa il file delle rotte delle vacanze
+const vacationRoutes = require('./routes/vacations');
+
+dotenv.config();
+
+const app = express();
+
+// Configura il middleware CORS
+app.use(cors({
+    origin: '*', // Sostituisci con il tuo dominio o frontend specifico
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true // Permette di inviare cookie insieme alla richiesta
+}));
+
+app.use(express.json());
+
+// Configura le rotte
+app.use('/api/vacations', vacationRoutes);
+
+// Connessione a MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+    const PORT = process.env.PORT || 5001;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+
+// Avvio del server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
