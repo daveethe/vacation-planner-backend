3922bc41e040e6b2e86b2b6a214cf7307dc83efc
daveethe <111148401+daveethe@users.noreply.github.com>
2024-09-18 19:49:36 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,32 +6,33 @@ const dotenv = require('dotenv');
 // Importa il file delle rotte delle vacanze
 const vacationRoutes = require('./routes/vacations');
 
-dotenv.config(); // Carica le variabili d'ambiente dal file .env
+dotenv.config();
 
 const app = express();
 
 // Configura il middleware CORS
 app.use(cors({
-    origin: 'https://daveethe.github.io', // Sostituisci con il tuo dominio o frontend specifico
+    origin: '*', // Sostituisci con il tuo dominio o frontend specifico
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true // Permette di inviare cookie insieme alla richiesta
 }));
 
-app.use(express.json()); // Middleware per il parsing del JSON
+app.use(express.json());
 
 // Configura le rotte
 app.use('/api/vacations', vacationRoutes);
 
 // Connessione a MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('Connected to MongoDB');
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
-        // Usa la variabile d'ambiente PORT assegnata da Render
-        const PORT = process.env.PORT || 3000;  // Utilizza 3000 come default solo in locale
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    })
-    .catch(err => console.error('Error connecting to MongoDB:', err));
+// Avvio del server
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
